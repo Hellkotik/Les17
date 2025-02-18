2a4cdc9dcde53771dc38ef49f737a6f92fa1e79f
Hellkotik <iglkotik@gmail.com>
2025-02-18 15:25:32 +0300

    Добавьте файлы проекта.

---
+++ Les17/Bank.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Les17
+{
+    class Bank<T>
+    {
+       internal T Check { get; set; }
+       internal double Balance { get; set; }
+       internal string Name { get; set; }
+
+       /* public Bank(T check, double balance, string name)
+        {
+            Check = check;
+            Balance = balance;
+            Name = name;
+        }
+       */
+
+        public void ReadB()
+    {
+        Console.WriteLine($"Ваши данные считаны: номер счета - {Check}, ваш баланс - {Balance}, ваше имя - {Name}");
+    }
+
+        public void Input()
+        {
+            
+            Check = (T)Convert.ChangeType(Console.ReadLine(), typeof(T));
+            Console.WriteLine("Введите 3 цифры с обратной стороны карточки");
+            Thread.Sleep(1000);
+            Console.WriteLine("Шутка, мы сами знаем эти цифры"+"\n");
+        }
+
+    }
+    
+
+}
---
+++ Les17/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Les17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+                Console.WriteLine("Введите ваш номер счета цифрами");
+                Bank<int> bank = new Bank<int>();
+                double balance = 300;
+                bank.Balance = balance;
+                string name = "Ivanoff";
+                bank.Name = name;
+        try { 
+                bank.Input();
+                bank.ReadB();
+                
+            }
+
+            catch
+            {
+                Console.WriteLine("Номер счета не состоял только из цифр. Попробуйте еще раз. Нажмите enter");
+                Console.ReadKey();
+            }
+            Console.WriteLine("Введите ваш номер счета буквами");
+            Bank<string> bank2 = new Bank<string>();
+            double balance2 = 10000000;
+            bank2.Balance = balance2;
+            string name2 = "NeIvanoff";
+            bank2.Name = name2;
+            try
+            {
+                bank2.Input(); bank2.ReadB();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+     
+        }
+    }
+}
